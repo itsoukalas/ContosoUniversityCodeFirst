744f9649f6514a01fc2b4e36d6f427ebb707e0d8
itsoukalas <73250253+itsoukalas@users.noreply.github.com>
2021-04-02 20:31:48 +0300

    >>Created a Details page >>Updated the Create page >>Updated the HttpPost Edit method >>Updated the Delete page >>Closed database connections >>Handled transactions

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -46,15 +46,27 @@ namespace ContosoUniversity.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        //prevent cross-site request forgery
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        //Removed ID is the primary key value which SQL Server will set automatically when the row is inserted
+
+        //The Bind attribute is one way to protect against over-posting 
+        public ActionResult Create([Bind(Include = "LastName,FirstMidName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Students.Add(student);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
             }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            
 
             return View(student);
         }
@@ -79,7 +91,10 @@ namespace ContosoUniversity.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+
+        /**Δημιουργήθηκε αυτόματα και το αλλαξα με το παρακάτω
+         * 
+         *  public ActionResult Edit([Bind(Include = "LastName,FirstMidName,EnrollmentDate")] Student student)
         {
             if (ModelState.IsValid)
             {
@@ -89,6 +104,36 @@ namespace ContosoUniversity.Controllers
             }
             return View(student);
         }
+
+
+
+         *
+         */
+
+        public ActionResult EditPost(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var studentToUpdate = db.Students.Find(id);
+            if (TryUpdateModel(studentToUpdate,"",new string[] { "ID","LastName","FirstMidName","EnrollmentDate"}))
+            {
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index","Students");
+                }
+                catch (DataException)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+            return View(studentToUpdate);
+        }
+
 
         // GET: Students/Delete/5
         public ActionResult Delete(int? id)
@@ -105,14 +150,52 @@ namespace ContosoUniversity.Controllers
             return View(student);
         }
 
-        // POST: Students/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+
+        // GET: Students/Delete/5
+
+        /**
+         *This code accepts an optional parameter that indicates 
+         *whether the method was called after a failure to save changes. 
+         *
+         *When it is called by the HttpPost Delete method in response to a database update error, 
+         *the parameter is true and an error message is passed to the view.
+         */
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
         {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
             Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+
+
+        // POST: Students/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Student student = db.Students.Find(id);
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
             return RedirectToAction("Index");
         }
 
--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
@@ -8,8 +9,11 @@ namespace ContosoUniversity.Models
 
         //primary key
         public int ID { get; set; }
+        [Display(Name ="Last Name")]
         public string LastName { get; set; }
+        [Display(Name ="First Name")]
         public string FirstMidName { get; set; }
+        [Display(Name ="Date")]
         public DateTime EnrollmentDate { get; set; }
 
 
@@ -17,6 +21,7 @@ namespace ContosoUniversity.Models
         //virtual->lazy loading - https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/reading-related-data-with-the-entity-framework-in-an-asp-net-mvc-application
         //ICollection- > declare a list of entries
 
+        [Display(Name ="Enrollment list")]
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
