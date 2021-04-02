3ce5050f1dcaaf4b76cf2c9c34c733fbb3c2b422
itsoukalas <73250253+itsoukalas@users.noreply.github.com>
2021-04-02 13:22:51 +0300

    Add project files.

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        /**
+         * If you don't specify a connection string or the name of one explicitly, 
+         * Entity Framework assumes that the connection string name is the same as the class name. 
+         */
+
+        public SchoolContext() : base()
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
